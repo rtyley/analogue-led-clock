9e2816684a2ba4aea622d5255bca16f996eaa06b
Roberto Tyley <roberto.tyley@gmail.com>
2026-02-15 09:04:17 +0000

    Boom

---
+++ pico-clock/device-fs/bang.py
@@ -0,0 +1,7 @@
+from holtek.ht1632c.driver import HT1632C
+from analogue_clock import AnalogueClock
+
+print('******HELLO***')
+
+ac = AnalogueClock(HT1632C(base_pin_index=2, state_machine_id=0, freq = 2000000))
+ac.initialise()
