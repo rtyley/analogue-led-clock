b7019b0ad0580e662241d8352afc1c758e63b0ef
Roberto Tyley <roberto.tyley@gmail.com>
2026-03-08 10:53:54 +0000

    Adapt `tzif-parser` code to run on MicroPython
    ## `datetime` library is required by `tzif-parser`
    
    We need to mip install `datetime`, tzif-parser requires it.
    
    https://github.com/micropython/micropython-lib/tree/master/python-stdlib/datetime
    
    ```
    mpremote connect id:a5f14b8bfbff4289 mip install datetime
    ```
    
    ## Missing features in MicroPython
    
    * `sysconfig`
    * `dataclasses`
      - https://docs.python.org/3/library/dataclasses.html
      - https://github.com/orgs/micropython/discussions/13741
      - https://github.com/dhrosa/udataclasses - https://udataclasses.readthedocs.io/en/latest/
    * `struct` (https://docs.python.org/3/library/struct.html#format-characters) features not
      available in MicroPython (https://docs.micropython.org/en/latest/library/struct.html#module-struct):
      - `c` for `char`
      - the repeat symbol `1`
    
    Decode version byte
    
    Why are we using this big regex?

--- pico-clock/device-fs/tzif_parser/tzif_header.py
+++ pico-clock/device-fs/tzif_parser/tzif_header.py
@@ -1,21 +1,28 @@
 import struct
-from dataclasses import dataclass
-from typing import IO
 
 
-@dataclass
 class TimeZoneInfoHeader:
-    version: int
-    is_utc_flag_count: int
-    wall_standard_flag_count: int
-    leap_second_transitions_count: int
-    transitions_count: int
-    local_time_type_count: int
-    timezone_abbrev_byte_count: int
+    def __init__(
+        self,
+        version,
+        is_utc_flag_count,
+        wall_standard_flag_count,
+        leap_second_transitions_count,
+        transitions_count,
+        local_time_type_count,
+        timezone_abbrev_byte_count,
+    ):
+        self.version = version
+        self.is_utc_flag_count = is_utc_flag_count
+        self.wall_standard_flag_count = wall_standard_flag_count
+        self.leap_second_transitions_count = leap_second_transitions_count
+        self.transitions_count = transitions_count
+        self.local_time_type_count = local_time_type_count
+        self.timezone_abbrev_byte_count = timezone_abbrev_byte_count
 
     @classmethod
-    def read(cls, file: IO[bytes]) -> "TimeZoneInfoHeader":
-        format_ = ">4s1c15x6I"
+    def read(cls, file):
+        format_ = ">4sb15x6I"
         header_size = struct.calcsize(format_)
         header_data = struct.unpack(format_, file.read(header_size))
         (
@@ -32,7 +39,7 @@ class TimeZoneInfoHeader:
         if magic != b"TZif":
             raise ValueError("Invalid TZif file: Magic sequence not found.")
 
-        version = 1 if version_byte == b"\x00" else int(version_byte.decode("ascii"))
+        version = 1 if version_byte == b"\x00" else version_byte - 48 # ord('0')
 
         return cls(
             version,
