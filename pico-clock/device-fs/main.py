7240309185ccbceccee753548012997eab7ab28a
Roberto Tyley <roberto.tyley@gmail.com>
2026-03-15 19:13:33 +0000

    Use `tzif-parser` to parse TZif files and get the timezone offset

--- pico-clock/device-fs/main.py
+++ pico-clock/device-fs/main.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 from machine import Pin, I2C
 import sys
 from DS3231.ds3231_gen import *
 from analogue_clock import AnalogueClock
 from holtek.ht1632c.driver import HT1632C
+from tzif_parser import TimeZoneInfo
 
 i2c = I2C(0, scl=Pin(13), sda=Pin(12))
 
@@ -13,6 +16,15 @@ print(d.get_time())
 d.alarm1.set(EVERY_SECOND)
 print('******HELLO I AM THE MAIN CODE***')
 
+timezone_name = open('/timezone.txt').readline().strip()
+print(f'timezone_name={timezone_name}')
+
+tz_info = TimeZoneInfo.read(timezone_name)
+
+print(tz_info)
+current_transition_index = tz_info.body.find_transition_index(datetime.now())
+print(current_transition_index)
+
 ac = AnalogueClock(HT1632C(base_pin_index=2, state_machine_id=0, freq = 10*1000*1000))
 ac.initialise()
 
@@ -22,8 +34,17 @@ try:
         while not d.alarm1():  # Wait for alarm
             pass
         YY, MM, DD, hh, mm, ss, wday, _ = d.get_time()
-        ac.light_time(hh, mm)
+
+        resolved = tz_info.resolve(datetime(YY, MM, DD, hh, mm, ss))
+        print(f'local_time={resolved.local_time}')
+        print(f'resolution_time={resolved.resolution_time}')
+        print(f'next_transition={resolved.next_transition}')
+        print(f'utc_offset_secs={resolved.utc_offset_secs}')
+
+        local_time = resolved.local_time
+        ac.light_time(local_time.hour, local_time.minute)
         time.sleep(0.3)  # Pin stays low for 300ms
+
 except Exception as e:
     print("Fatal error in main:")
     sys.print_exception(e)
