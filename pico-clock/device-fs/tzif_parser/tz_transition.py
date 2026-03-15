822116a2e15bd3b9ddab56033307f4f00720c0ba
Roberto Tyley <roberto.tyley@gmail.com>
2026-03-15 19:11:27 +0000

    Parse TZif: Copy-in https://github.com/MasonFlint44/tzif-parser at commit 3fb54ecf
    The code is copied at commit 3fb54ecf3f9df5aeff1a36ed953f18a790aaf76f, and MIT-licensed:
    
    https://github.com/MasonFlint44/tzif-parser/blob/3fb54ecf3f9df5aeff1a36ed953f18a790aaf76f/LICENSE
    
    This is the specific folder I copied - I left out the tests etc:
    
    https://github.com/MasonFlint44/tzif-parser/tree/3fb54ecf3f9df5aeff1a36ed953f18a790aaf76f/tzif_parser
    
    Note that this code works with CPython, but not yet with MicroPython.

---
+++ pico-clock/device-fs/tzif_parser/tz_transition.py
@@ -0,0 +1,109 @@
+from datetime import datetime, timedelta, timezone
+
+from .models import TimeTypeInfo, WallStandardFlag
+
+
+class TimeZoneTransition:
+    def __init__(
+        self,
+        transition_time: datetime,
+        time_type_infos: list[TimeTypeInfo],
+        time_type_indices: list[int],
+        transition_index: int,
+        wall_standard_flags: list[int],
+        is_utc_flags: list[int],
+        timezone_abbrevs: str,
+    ) -> None:
+        self._transition_time = transition_time
+        self._time_type_infos = time_type_infos
+        self._time_type_indices = time_type_indices
+        self._transition_index = transition_index
+        self.time_type_info = time_type_infos[time_type_indices[transition_index]]
+        self.wall_standard_flag = (
+            WallStandardFlag(wall_standard_flags[time_type_indices[transition_index]])
+            if len(wall_standard_flags) > 0
+            else None
+        )
+        self.is_utc = (
+            bool(is_utc_flags[time_type_indices[transition_index]])
+            if len(is_utc_flags) > 0
+            else None
+        )
+        self.abbreviation = timezone_abbrevs[
+            self.time_type_info.abbrev_index :
+        ].partition("\x00")[0]
+
+    @property
+    def transition_time_local_standard(self) -> datetime:
+        if self._transition_index == 0:
+            # Prefer a non-DST ttinfo if present, else fall back to index 0
+            first_std = next(
+                (tti for tti in self._time_type_infos if not tti.is_dst),
+                self._time_type_infos[0],
+            )
+            return self.transition_time_utc.astimezone(
+                timezone(timedelta(seconds=first_std.utc_offset_secs))
+            ).replace(tzinfo=None)
+        ttinfo = self._time_type_infos[
+            self._time_type_indices[self._transition_index - 1]
+        ]
+        return self.transition_time_utc.astimezone(
+            timezone(timedelta(seconds=ttinfo.utc_offset_secs))
+        ).replace(tzinfo=None)
+
+    @property
+    def transition_time_local_wall(self) -> datetime:
+        return self.transition_time_utc.astimezone(
+            timezone(timedelta(seconds=self.utc_offset_secs))
+        ).replace(tzinfo=None)
+
+    @property
+    def transition_time_utc(self) -> datetime:
+        return self._transition_time
+
+    @property
+    def dst_difference_secs(self) -> int:
+        if not self.is_dst:
+            return 0
+
+        # Prefer the preceding standard ttinfo; fall back to next, then any non-DST entry.
+        std_tt: TimeTypeInfo | None = None
+        if self._transition_index > 0:
+            prev_tt = self._time_type_infos[
+                self._time_type_indices[self._transition_index - 1]
+            ]
+            if not prev_tt.is_dst:
+                std_tt = prev_tt
+
+        if std_tt is None and self._transition_index + 1 < len(self._time_type_indices):
+            next_tt = self._time_type_infos[
+                self._time_type_indices[self._transition_index + 1]
+            ]
+            if not next_tt.is_dst:
+                std_tt = next_tt
+
+        if std_tt is None:
+            std_tt = next(
+                (tti for tti in self._time_type_infos if not tti.is_dst), None
+            )
+
+        if std_tt is None:
+            return 0
+
+        return self.utc_offset_secs - std_tt.utc_offset_secs
+
+    @property
+    def dst_difference_hours(self) -> float:
+        return self.dst_difference_secs / 3600
+
+    @property
+    def utc_offset_secs(self) -> int:
+        return self.time_type_info.utc_offset_secs
+
+    @property
+    def utc_offset_hours(self) -> float:
+        return self.utc_offset_secs / 3600
+
+    @property
+    def is_dst(self) -> bool:
+        return self.time_type_info.is_dst
